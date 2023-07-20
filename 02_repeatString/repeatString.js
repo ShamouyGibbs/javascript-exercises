af96c4ea90af2536c44f0fa60a60d613047268ff
Shamouy Gibbs <dev.shamouy>
2023-07-20 15:47:56 -0500

    Completed the first 2 javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
 
+    if(num > -1){
+        for(let i = 0; i < num; i++){
+            newString += string;
+        }
+        
+        return newString;
+
+    } else return "ERROR"
+   
+    
 };
 
 // Do not edit below this line
