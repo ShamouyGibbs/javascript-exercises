be0be1059e282beeabbc5526036097dc3a931abf
Shamouy Gibbs <dev.shamouy>
2023-07-20 19:04:13 -0500

    Completed JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(strings) {
+    let string = strings;
+    let stringLength = string.length
+    let newString = "";
 
+    for (let i = 1; i <= strings.length; i++){
+
+        newString += string[stringLength-i];
+        
+    }
+   
+        return newString;
 };
 
 // Do not edit below this line
