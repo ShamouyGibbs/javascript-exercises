fc35610359405249533aa78a871626a713483404
Shamouy Gibbs <dev.shamouy>
2023-07-26 00:16:40 -0500

    Completed the javascript exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(itemsList, ...items){
+     
+    for (let item of items){
+        
+        if (item === itemsList[itemsList.indexOf(item)]){
+            itemsList.splice(itemsList.indexOf(item), 1);  
+        }
+    }
+    
+    return itemsList;
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
