4d303e281eba46886860fe30c1bd9bb035ed273b
Shamouy Gibbs <dev.shamouy>
2023-07-26 02:17:11 -0500

    remove some redundant code.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1, num2) {
     
     let sum = 0;
-    
+
     if (num1 < 0 || num2 < 0){return "ERROR"}
 
     if (typeof num1 && typeof num2 === 'number'){
@@ -13,8 +13,6 @@ const sumAll = function(num1, num2) {
             } return sum;  
             
         }
-        
-        if (num1 && num2 < 0){ return "ERROR"}
 
         if (num1 < num2){
             for (let i = num1; i <= num2; i++){
