70a1aa5547c8bc50697fa325a4a90c20b2406ecf
Shamouy Gibbs <dev.shamouy>
2023-07-26 02:15:49 -0500

    Completed javascript exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let sum = 0;
+    
+    if (num1 < 0 || num2 < 0){return "ERROR"}
+
+    if (typeof num1 && typeof num2 === 'number'){
+        
+        if (num1 > num2){
+            for (let i = num2; i <= num1; i++){
+                sum += i;
+                
+            } return sum;  
+            
+        }
+        
+        if (num1 && num2 < 0){ return "ERROR"}
+
+        if (num1 < num2){
+            for (let i = num1; i <= num2; i++){
+                sum += i;
+                
+            } return sum; 
+    } 
+
+  } else return "ERROR"
 
 };
 
